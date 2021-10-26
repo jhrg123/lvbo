977fc9e661288d7f8aeace6b23c09461ac07031b
jhrg123 <51513057+jhrg123@users.noreply.github.com>
2021-10-26 20:14:38 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pandas as pd
 
+
 # file=open("uav1.txt")
 # lines=file.readlines()
 # row=len(lines)
 # print(row)
 
 df= pd.read_csv("uav1.txt",sep="  ",header=None,names=["t","a","b","c"],dtype=np.float64)
-print(df.shape)
-
+df_array=np.array(df)
+print(np.dot(df_array.T,df_array)[1,:])
