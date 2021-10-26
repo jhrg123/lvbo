4d4a4c52c20363fccddc46914c7f228ea31eed8f
jhrg123 <51513057+jhrg123@users.noreply.github.com>
2021-10-26 21:33:17 +0800

    asd

--- main.py
+++ main.py
@@ -15,3 +15,6 @@ targe_mat=np.mat(pd.read_csv("target.txt",sep="  ",header=None,dtype=np.float64)
 measur_mat=np.mat(pd.read_csv("measurement.txt",sep="  ",header=None,dtype=np.float64))
 
 
+# 初始化状态变量
+status=np.array([[0,0,0,47.8109,18.1173,47.8109,0,0,0]]).T
+print(status)
