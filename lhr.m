ddb1c222f8700affc1958b7d1112b2af15d7acf3
jhrg123 <51513057+jhrg123@users.noreply.github.com>
2021-10-26 22:19:31 +0800

    asd

--- lhr.m
+++ lhr.m
@@ -1,7 +1,7 @@
 a=eye(6);
 b=zeros(6,3);
 c=zeros(3,9);
-A=[b,a;c];
-eye(9)+A+A^2
+A=[b,a;c]
+% eye(9)+A+A^2
 
 
--- main.py
+++ main.py
@@ -11,7 +11,12 @@ uav2=np.mat(pd.read_csv("uav2.txt",sep="  ",header=None,names=["t","a","b","c"],
 targe_mat=np.mat(pd.read_csv("target.txt",sep="  ",header=None,dtype=np.float64))
 measur_mat=np.mat(pd.read_csv("measurement.txt",sep="  ",header=None,dtype=np.float64))
 
+T=0.1
 
 # 初始化状态变量
 status0=np.array([[0,0,0,47.8109,18.1173,47.8109,0,0,0]]).T
+A=np.c_[np.zeros([9,3]),np.r_[np.eye(6),np.zeros([3,6])]]
+fai=np.eye(9)+T*A+T**2/2*np.matmul(A,A)
+print(fai)
+
 
